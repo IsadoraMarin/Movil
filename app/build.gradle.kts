347705e325140d0d81d824a6d32b16746adbdd58
IsadoraMarin <icarolinamaring@gmail.com>
2025-11-10 17:17:17 -0300

    - Estructura base para REST API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,13 @@ android {
 
 dependencies {
 
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("org.jetbrains.kotlix:kotlinx-courutines-android:1.8.0")
+
     // Navigation para Jetpack Compose
     implementation("androidx.navigation:navigation-compose:2.8.0")
 
