5aeebe988009711c353ee349d943f4445af889e5
BenjamínGóngora <be.gongora@duocuc.cl>
2025-11-12 19:59:39 -0300

    Implementar pantalla de edición de perfil

--- app/src/main/java/com/example/proyectoaplicaciones/ui/Screens/EditProfileScreen.kt
+++ app/src/main/java/com/example/proyectoaplicaciones/ui/Screens/EditProfileScreen.kt
@@ -3,7 +3,7 @@ package com.example.proyectoaplicaciones.ui.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape 
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
