928c1974a7cd108ef8a07c2b06f9b0a8b5c28013
BenjamínGóngora <be.gongora@duocuc.cl>
2025-11-12 19:56:13 -0300

    Implementar pantalla de registro de cuenta

--- app/src/main/java/com/example/proyectoaplicaciones/ui/Screens/RegisterScreen.kt
+++ app/src/main/java/com/example/proyectoaplicaciones/ui/Screens/RegisterScreen.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons 
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.* 
 import androidx.compose.runtime.*
