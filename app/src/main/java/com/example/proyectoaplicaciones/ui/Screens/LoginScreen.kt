af49001bc9f1987f5a0c490d0e0ac56e44873307
BenjamínGóngora <be.gongora@duocuc.cl>
2025-11-12 20:01:54 -0300

    Implementar pantalla de inicio de sesión

--- app/src/main/java/com/example/proyectoaplicaciones/ui/Screens/LoginScreen.kt
+++ app/src/main/java/com/example/proyectoaplicaciones/ui/Screens/LoginScreen.kt
@@ -1,6 +1,6 @@
 package com.example.proyectoaplicaciones.ui.screens
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.background 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
