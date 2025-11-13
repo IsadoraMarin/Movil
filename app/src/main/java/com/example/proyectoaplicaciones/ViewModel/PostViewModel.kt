7a8d124591db4f24ce776179dc9c302124946bbd
BenjamínGóngora <be.gongora@duocuc.cl>
2025-11-12 21:06:37 -0300

    Reestructurar PostViewModel y añadir función de borrado

--- app/src/main/java/com/example/proyectoaplicaciones/ViewModel/PostViewModel.kt
+++ app/src/main/java/com/example/proyectoaplicaciones/ViewModel/PostViewModel.kt
@@ -1,72 +1,33 @@
-package com.example.proyectoaplicaciones.ViewModel
+package com.example.proyectoaplicaciones.Repository
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.example.proyectoaplicaciones.Data.Model.Comentarios
 import com.example.proyectoaplicaciones.Data.Model.Post
-import com.example.proyectoaplicaciones.Repository.PostRepository
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import com.example.proyectoaplicaciones.Data.Model.Rating
+import com.example.proyectoaplicaciones.Data.Remote.RetroFitInstance
 
-class PostViewModel : ViewModel(){
-    private val repository = PostRepository()
-
-    private val _popularPosts = MutableStateFlow<List<Post>>(emptyList())
-    val popularPosts: StateFlow<List<Post>> = _popularPosts
-
-    private val _newsPosts = MutableStateFlow<List<Post>>(emptyList())
-    val newsPosts: StateFlow<List<Post>> = _newsPosts
-
-    private val _communityPosts = MutableStateFlow<List<Post>>(emptyList())
-    val communityPosts: StateFlow<List<Post>> = _communityPosts
-
-    private val _postList = MutableStateFlow<List<Post>>(value = emptyList())
-
-    val postList: StateFlow<List<Post>> = _postList
-
-    init {
-        fetchPosts()
+class PostRepository{
+    private val apiService = RetroFitInstance.api
+    suspend fun getPostsByCategory(Categoria: String): List<Post>{
+        return apiService.getPostsByCategory(Categoria)
     }
 
-    private fun fetchPosts(){
-        viewModelScope.launch {
-            try {
-                _popularPosts.value = repository.getPostsByCategory("Popular")
-                _newsPosts.value = repository.getPostsByCategory("Noticias")
-                _communityPosts.value = repository.getPostsByCategory("Comunidades")
-                _postList.value = _popularPosts.value + _newsPosts.value + _communityPosts.value
-            }catch (e: Exception){
-                println("Error al obtener los datos: ${e.localizedMessage}")
-            }
-        }
+    suspend fun addPost(post: Post) {
+        apiService.addPost(post)
     }
 
-    fun publishPost(title: String, content: String, autor: String, category: String) {
-        viewModelScope.launch {
-            try {
-                val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss", Locale.getDefault())
-                val currentDate = sdf.format(Date())
-                val newPost = Post(id = 0, title = title, content = content, categoria = category, autor = autor, date = currentDate)
-                repository.addPost(newPost)
-                fetchPosts() // refrescar después de agregar
-            } catch (e: Exception) {
-                println("Error al publicar post: ${e.localizedMessage}")
-            }
-        }
+    suspend fun addComment(postId: Int, comment: String, author: String) {
+        apiService.addComment(postId, Comentarios(0, author, "", comment))
     }
 
-    fun addComment(postId: Int, comment: String, autor: String) {
-        viewModelScope.launch {
-            try {
-                repository.addComment(postId, comment, autor)
-                fetchPosts() // refrescar para mostrar comentarios actualizados
-            } catch (e: Exception) {
-                println("Error al agregar comentario: ${e.localizedMessage}")
-            }
-        }
+    suspend fun updatePost(postId: Int, post: Post) {
+        apiService.updatePost(postId, post)
     }
 
+    suspend fun deletePost(postId: Int) {
+        apiService.deletePost(postId)
+    }
+
+    suspend fun ratePost(postId: Int, value: Boolean) {
+        apiService.ratePost(postId, Rating(value))
+    }
 }
