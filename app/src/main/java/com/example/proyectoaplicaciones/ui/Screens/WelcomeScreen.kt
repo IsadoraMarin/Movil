fcc25d155b877d99a3cc67a78d1c9298d1a32c21
BenjamínGóngora <be.gongora@duocuc.cl>
2025-11-13 00:43:39 -0300

    Update WelcomeScreen.kt

--- app/src/main/java/com/example/proyectoaplicaciones/ui/Screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/proyectoaplicaciones/ui/Screens/WelcomeScreen.kt
@@ -2,21 +2,20 @@ package com.example.proyectoaplicaciones.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Forum
 import androidx.compose.material3.* 
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.proyectoaplicaciones.Navigation.Screen
-import com.example.proyectoaplicaciones.ViewModel.AuthViewModel
+import com.example.proyectoaplicaciones.viewmodel.AuthViewModel
 
 @Composable
 fun WelcomeScreen(navController: NavController, authViewModel: AuthViewModel) {
