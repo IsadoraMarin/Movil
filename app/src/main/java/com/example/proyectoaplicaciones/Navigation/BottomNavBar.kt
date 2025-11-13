58969c1305359c7180d2e6fa21ba17b7055e6559
BenjamínGóngora <be.gongora@duocuc.cl>
2025-11-12 21:34:02 -0300

    Crear componente reutilizable BottomNavBar para los botones de navegacion.

--- app/src/main/java/com/example/proyectoaplicaciones/Navigation/BottomNavBar.kt
+++ app/src/main/java/com/example/proyectoaplicaciones/Navigation/BottomNavBar.kt
@@ -1,68 +1,56 @@
 package com.example.proyectoaplicaciones.Navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Comment
+import androidx.compose.material.icons.filled.Newspaper
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
-
-@Composable
-fun BottomNavBar(navController: NavController){
-    val items = listOf(
-        BottomNavItem(
-            name = "Home",
-            route = Screen.Home.route,
-            icon = Icons.Default.Home
-        ),
-        BottomNavItem(
-            name = "Populares",
-            route = Screen.Popular.route,
-            icon = Icons.Default.Favorite
-        ),
-        BottomNavItem(
-            name = "Noticias",
-            route = Screen.Noticias.route,
-            icon = Icons.Default.List
-        ),
-        BottomNavItem(
-            name = "Comunidad",
-            route = Screen.Comunidad.route,
-            icon = Icons.Default.Face
-        )
-    )
-
-    NavigationBar{
-        val navBackStackEntry = navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry.value?.destination?.route
-
-        items.forEach { item ->
-            NavigationBarItem(
-                icon = {Icon(item.icon, contentDescription = item.name)},
-                label = {Text(item.name)},
-                selected = currentRoute == item.route,
-                onClick = {
-                    if(currentRoute != item.route){
-                        navController.navigate(item.route){
-                            popUpTo(navController.graph.startDestinationId)
-                            launchSingleTop = true
-                        }
-                    }
-                }
-            )
-        }
-    }
-}
 
 data class BottomNavItem(
     val name: String,
     val route: String,
     val icon: ImageVector
 )
+
+@Composable
+fun BottomNavBar(navController: NavController) {
+    val items = listOf(
+        BottomNavItem("Populares", Screen.Popular.route, Icons.Default.Star),
+        BottomNavItem("Noticias", Screen.Noticias.route, Icons.Default.Newspaper),
+        BottomNavItem("Comunidad", Screen.Comunidad.route, Icons.Default.Comment),
+        BottomNavItem("Perfil", Screen.Profile.route, Icons.Default.AccountCircle)
+    )
+
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(item.icon, contentDescription = item.name) },
+                label = { Text(item.name) },
+                selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
