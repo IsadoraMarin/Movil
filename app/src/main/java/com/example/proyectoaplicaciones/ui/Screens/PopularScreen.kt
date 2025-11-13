725c97c44c94ae369bf67c0de8e100c7f182ede5
BenjamínGóngora <be.gongora@duocuc.cl>
2025-11-12 23:05:07 -0300

    Update PopularScreen.kt

--- app/src/main/java/com/example/proyectoaplicaciones/ui/Screens/PopularScreen.kt
+++ app/src/main/java/com/example/proyectoaplicaciones/ui/Screens/PopularScreen.kt
@@ -1,4 +1,4 @@
-package com.example.proyectoaplicaciones.ui.Screens
+package com.example.proyectoaplicaciones.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
